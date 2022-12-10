9182117fa9ab845590dc837779287c4abd5bae47
deonkev <aolasupo0@gmail.com>
2022-12-09 18:12:37 -0800

    task 5 and task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -print all single digit numbers of base 10 starting from '0'
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
+
+	putchar('\n')
+	return (0);
+}
